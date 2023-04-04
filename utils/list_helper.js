0e8b76ba050129c39e7c69dc6df46248be9a38c7
AUVTE0 <66396373+AUVTE0@users.noreply.github.com>
2023-04-04 14:47:33 +0100

    exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) =>{
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.map(blog => blog.totalLikes).reduce((accum, current)=> accum+current, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
--- utils/list_helper.test.js
+++ utils/list_helper.test.js
@@ -1,7 +1,28 @@
-const listHelper = require('../utils/list_helper')
+const {dummy, totalLikes} = require('../utils/list_helper')
 
 test('dummy returns 1', () => {
     const blogs = []
-    const result = listHelper.dummy(blogs)
+    const result = dummy(blogs)
     expect(result).toBe(1)
+})
+
+describe('total likes', () =>{
+    test('of empty list is zero', () => {
+        const blogs = []
+        expect(totalLikes(blogs)).toBe(0)
+    })
+
+    test('when list has only one blog equals likes of that', () => {
+        const blogs = [ { totalLikes: 5 } ]
+        expect(totalLikes(blogs)).toBe(5)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        const blogs = [ 
+            { totalLikes: 1 },
+            { totalLikes: 3 },
+            { totalLikes: 5 },
+        ]
+        expect(totalLikes(blogs)).toBe(9)
+    })
 })
