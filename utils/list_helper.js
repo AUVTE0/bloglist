73d5d1f08de12405450dc0a2b5ee2577fbc7a8c2
AUVTE0 <66396373+AUVTE0@users.noreply.github.com>
2023-04-04 15:14:02 +0100

    exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,10 +3,16 @@ const dummy = (blogs) =>{
 }
 
 const totalLikes = (blogs) => {
-    return blogs.map(blog => blog.totalLikes).reduce((accum, current)=> accum+current, 0)
+    return blogs.map(blog => blog.likes).reduce((accum, current)=> accum+current, 0)
+}
+
+const favouriteBlog = (blogs) => {
+    return blogs.length === 0? null
+        :blogs.sort((a, b) => a.likes > b.likes)[0]
 }
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
 }
