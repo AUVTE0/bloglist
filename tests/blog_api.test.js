f50337899b339e74cf7e0f5a3bc19878548047e6
AUVTE0 <66396373+AUVTE0@users.noreply.github.com>
2023-04-05 13:59:19 +0100

    exercise 4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,14 +1,28 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
+const helper = require('./test_helper')
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    const promiseArray = helper.initialBlogs.map(b => (new Blog(b)).save())
+    await Promise.all(promiseArray)
+})
+
 
 test('blogs are returned as json', async () => {
     await api
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
+})
+
+test('blogs UID property is named id', async () => {
+    const blogs = await helper.blogsInDb()
+
+    expect(blogs[0].id).toBeDefined()
 })
 
 afterAll(async () => {
